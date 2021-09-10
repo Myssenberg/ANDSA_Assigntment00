01ec096a62d2c2d1b69b327c43a8306e7b0241b0
Myssenberg <myje@itu.dk>
2021-09-10 09:55:47 +0200

    Tested and implemented IsLeapYear

--- Assignment00.Tests/ProgramTests.cs
+++ Assignment00.Tests/ProgramTests.cs
@@ -6,9 +6,55 @@ namespace Assignment00.Tests
     public class ProgramTests
     {
         [Fact]
-        public void Test1()
+        public void IsLeapYear_accepts_divisible_by_four()
         {
+            //Arrange
+            Program test = new Program();
+
+            //Act
+            bool result = test.IsLeapYear(1996);
             
+            //Assert
+            Assert.True(result);
+        }
+
+		[Fact]
+        public void IsLeapYear_doesnt_accept_not_divisible_by_four()
+        {
+            //Arrange
+            Program test = new Program();
+
+            //Act
+            bool result = test.IsLeapYear(1995);
+            
+            //Assert
+            Assert.False(result);
+        }
+
+		[Fact]
+        public void IsLeapYear_doesnt_accept_divisible_by_100()
+        {
+            //Arrange
+            Program test = new Program();
+
+            //Act
+            bool result = test.IsLeapYear(1900);
+            
+            //Assert
+            Assert.False(result);
+        }
+
+		[Fact]
+        public void IsLeapYear_accepts_divisible_by_400()
+        {
+            //Arrange
+            Program test = new Program();
+
+            //Act
+            bool result = test.IsLeapYear(2000);
+            
+            //Assert
+            Assert.True(result);
         }
     }
 }
