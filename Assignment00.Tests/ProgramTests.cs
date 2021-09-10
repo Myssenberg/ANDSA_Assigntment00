acc716d0539b72dcc88980481add8721aef0e9c7
Myssenberg <myje@itu.dk>
2021-09-10 09:10:26 +0200

    Implemented program.cs and programtests.cs

---
+++ Assignment00.Tests/ProgramTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Assignment00.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
